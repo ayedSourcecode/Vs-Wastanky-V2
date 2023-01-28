9ca12a98b09e6df50f4b3a39789dce2ee1f18174
ayedFNFmakerSource code <123101780+ayedSourcecode@users.noreply.github.com>
2023-01-28 09:54:04 +0100

    Update MainMenuState.hx

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -25,7 +25,7 @@ using StringTools;
 
 class MainMenuState extends MusicBeatState
 {
-	public static var WastankyVersions:String = 'V2';
+	public static var WastankyVersion:String = 'V2'; //versions mods llool
 	public static var psychEngineVersion:String = '0.5.2h'; //This is also used for Discord RPC
 	public static var curSelected:Int = 0;
 
